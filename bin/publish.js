0b5cf0c4d56237a7702cb1ce55fc8d9e73db8fbc
spirit <catchspirit37@gmail.com>
2018-05-23 15:55:51 +0800

    0.0.2 @Wed May 23 2018 15:53:34 GMT+0800 (CST)

--- bin/publish.js
+++ bin/publish.js
@@ -6,6 +6,7 @@ ssh = new node_ssh()
 const path = require('path')
 const crypto = require('crypto')
 const childProcess = require('child_process')
+const start = Date.now()
 
 childProcess.execSync(`
     git checkout publish
@@ -13,12 +14,22 @@ childProcess.execSync(`
 const localApkPath = path.resolve(__dirname,'../android/app/build/outputs/apk/app-release.apk')
 const algorithm =  crypto.createHash('md5')
 let hashValue = algorithm.update(fs.readFileSync(localApkPath)).digest('hex')
+const version = require('../package.json').version
 let updateInfo = {
     hash:hashValue,
-    version:require('../package.json').version
+    version
 }
 const localUpdatePath = __dirname+'/update.json'
 fs.writeFileSync(localUpdatePath,JSON.stringify(updateInfo),'utf8')
-childProcess.execSync(`
-    npm run pack:android && git commit -am "${new Date()}" && git push
-`)
+childProcess.exec(`
+    npm run pack:android && git commit -am "${version} @${new Date()}" && git push
+`,(error,stdout,stderr)=>{
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    console.log(`time elapsed ${(Date.now()-start)/1000} s`)
+    
+})
