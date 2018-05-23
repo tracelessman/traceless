e0b76b99490a307fbf4c73d9c0ef9eae23f33593
spirit <catchspirit37@gmail.com>
2018-05-23 15:50:47 +0800

    Wed May 23 2018 15:48:53 GMT+0800 (CST)

--- bin/publish.js
+++ bin/publish.js
@@ -7,8 +7,9 @@ const path = require('path')
 const crypto = require('crypto')
 const childProcess = require('child_process')
 
-
-
+childProcess.execSync(`
+    git checkout publish
+`)
 const localApkPath = path.resolve(__dirname,'../android/app/build/outputs/apk/app-release.apk')
 const algorithm =  crypto.createHash('md5')
 let hashValue = algorithm.update(fs.readFileSync(localApkPath)).digest('hex')
@@ -18,3 +19,6 @@ let updateInfo = {
 }
 const localUpdatePath = __dirname+'/update.json'
 fs.writeFileSync(localUpdatePath,JSON.stringify(updateInfo),'utf8')
+childProcess.execSync(`
+    npm run pack:android && git commit -am "${new Date()}" && git push
+`)
