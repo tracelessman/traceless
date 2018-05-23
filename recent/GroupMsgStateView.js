e2bccfb9041955444c7dca9eac6d6e5921063bb4
randomyige <randomyige@gmail.com>
2018-05-23 09:56:30 +0800

    tmp

--- recent/GroupMsgStateView.js
+++ recent/GroupMsgStateView.js
@@ -21,7 +21,19 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 export default class GroupMsgStateView extends Component<{}> {
     constructor(props) {
         super(props);
+        this.state={states:null};
+    }
 
+    componentDidMount=()=>{
+        var gid = this.props.navigation.state.params.gid;
+        var msgId = this.props.navigation.state.params.msgId;
+        Store.getGroupChatRecord(gid,msgId,null, (rec)=> {
+            if(rec){
+                Store.getRecordStateReports(gid,msgId,(states)=>{
+                    this.setState({rec:rec,states:states});
+                });
+            }
+        });
     }
 
     getIconNameByState=function (state) {
@@ -42,26 +54,34 @@ export default class GroupMsgStateView extends Component<{}> {
     }
 
     render(){
-        var gid = this.props.navigation.state.params.gid;
-        var msgId = this.props.navigation.state.params.msgId;
-        var members = Store.getGroup(gid).members;
-        var rec = Store.getGroupChatRecord(gid,msgId);
-
+        var rec = this.state.rec;
         var friends = [];
-        for(var i=0;i<members.length;i++){
-            var f = members[i];
-            if(f.uid!=Store.getCurrentUid()){
-                var state = rec.states[f.uid]||rec.state;
-                var iconName = this.getIconNameByState(state);
-                friends.push(<TouchableOpacity key={i}  style={{width:"100%",flexDirection:"row",justifyContent:"center"}}>
-                    <View style={{flexDirection:"row",justifyContent:"space-around",alignItems:"center",width:"90%",height:40,marginTop:20}}>
-                        <Text>    {f.name}  </Text>
-                        <Ionicons name={iconName} size={20}  style={{marginRight:5,lineHeight:40}}/>
-                    </View>
-                </TouchableOpacity>);
-                friends.push(<View key={i+"line"} style={{width:"90%",height:0,borderTopWidth:1,borderColor:"#d0d0d0"}}></View>);
+
+        if(rec){
+            var gid = this.props.navigation.state.params.gid;
+            var members = Store.getGroup(gid).members;
+            var states = this.state.states;
+            for(var i=0;i<members.length;i++){
+                var f = members[i];
+                if(f.uid!=Store.getCurrentUid()){
+                    var state = rec.state;
+                    for(var j=0;j<states.length;j++){
+                        if(states[j].reporterUid == f.uid){
+                            state = states[j].state;
+                        }
+                    }
+                    var iconName = this.getIconNameByState(state);
+                    friends.push(<TouchableOpacity key={i}  style={{width:"100%",flexDirection:"row",justifyContent:"center"}}>
+                        <View style={{flexDirection:"row",justifyContent:"space-around",alignItems:"center",width:"90%",height:40,marginTop:20}}>
+                            <Text>    {f.name}  </Text>
+                            <Ionicons name={iconName} size={20}  style={{marginRight:5,lineHeight:40}}/>
+                        </View>
+                    </TouchableOpacity>);
+                    friends.push(<View key={i+"line"} style={{width:"90%",height:0,borderTopWidth:1,borderColor:"#d0d0d0"}}></View>);
+                }
             }
         }
+
         return (
             <View style={{flex:1,flexDirection:"column",justifyContent:"flex-start",alignItems:"center",backgroundColor:"#ffffff"}}>
 
