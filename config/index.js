453685435fcddc3110e2051151ae0c008e311eb2
spirit <catchspirit37@gmail.com>
2018-06-05 15:57:28 +0800

    ipa url

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const appName = "traceless"
 const publishFolderName = "pkg"
-const baseUrl = `https://github.com/tracelessman/traceless/raw/publish/${publishFolderName}/`
+const baseUrl = `https://raw.githubusercontent.com/tracelessman/traceless/publish/${publishFolderName}/`
 
 module.exports = {
     apkUrl:`${baseUrl+appName}.apk`,
