244e7431d4cff6be5075b1d5d811499f237f88a4
spirit <catchspirit37@gmail.com>
2018-06-10 22:25:37 +0800

    0.1.5

--- mine/MineView.js
+++ mine/MineView.js
@@ -111,7 +111,7 @@ export default class MineView extends Component<{}> {
                 onPress:this.showScanView
             },
             {
-                title:`当前版本:${versionLocal}-test`,
+                title:`当前版本:${versionLocal}`,
                 icon:'new-releases',
             },
         ]
