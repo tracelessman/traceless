f6f7e121609e7c372febe45f7ed0fc1aa06b007c
spirit <catchspirit37@gmail.com>
2018-07-03 22:07:20 +0800

    tmp

--- config/devConfig.js
+++ config/devConfig.js
@@ -1,15 +1,15 @@
 const path = require('path')
 const rootPath = path.resolve(__dirname,'../')
 const devDiffConfig = require("./devDiffConfig")
-
+const fs = require('fs')
 const devConfig = {
+    ...JSON.parse(fs.readFileSync(path.resolve(__dirname,'common.json'),"utf8").trim()),
     ...devDiffConfig,
-    appId:"traceless",
-    appName:"LK",
     localIpaFolderPath:path.resolve(rootPath,'pkg'),
     exportIpaFolderPath:path.resolve(devDiffConfig.localServerPublicFolderPath,"pkg/ios"),
     exportPpkFolderPath:path.resolve(devDiffConfig.localServerPublicFolderPath,"pkg/ppk"),
     exportAndroidFolderPath:path.resolve(devDiffConfig.localServerPublicFolderPath,"pkg/android"),
-    publishBranch:"publish",
 }
+console.log(devConfig)
+
 module.exports = devConfig
